1999c8b221965bc3e3855eff9cf7b1875380707a
titod <pratito99@gmail.com>
2019-02-20 13:13:01 +0700

    su

--- app/src/main/java/com/example/cobafragment/MainActivity.java
+++ app/src/main/java/com/example/cobafragment/MainActivity.java
@@ -6,21 +6,52 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  implements SimpleFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity
+        implements SimpleFragment.OnFragmentInteractionListener{
+    private Button mButton;
+    private boolean isFragmentDisplayed=false;
+
+    private int mRadioButtonChoice=2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton=findViewById(R.id.open_button);
+
     }
 
     public void openClick(View view) {
+        if(!isFragmentDisplayed){
+            displayFragment();
+        }else{
+            closeFragment();
+        }
+    }
+    public void displayFragment(){
         SimpleFragment simpleFragment=new SimpleFragment();
         FragmentManager fragmentManager=getSupportFragmentManager();
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment_container,simpleFragment).commit();
+
+        mButton.setText(R.string.close);
+        isFragmentDisplayed=true;
+    }
+
+    public void closeFragment(){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        SimpleFragment simpleFragment=
+                (SimpleFragment)fragmentManager.findFragmentById(R.id.fragment_container);
+        if(simpleFragment!=null){
+            FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        mButton.setText(R.string.open);
+        isFragmentDisplayed=false;
     }
 
     @Override
--- app/src/main/java/com/example/cobafragment/SimpleFragment.java
+++ app/src/main/java/com/example/cobafragment/SimpleFragment.java
@@ -22,12 +22,8 @@ import android.widget.TextView;
 public class SimpleFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String CHOICE = "choice";
+    private int mRadioButtonChoice=2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,20 +31,11 @@ public class SimpleFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SimpleFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static SimpleFragment newInstance(String param1, String param2) {
+    public static SimpleFragment newInstance(int choice) {
         SimpleFragment fragment = new SimpleFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(CHOICE, choice);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,10 +43,7 @@ public class SimpleFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        
     }
 
     @Override
